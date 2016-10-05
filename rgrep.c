da68e4b33b58fbecb9b151f072e7b2a1d01ebdc2
RoboMWM <RoboMWM@users.noreply.github.com>
2016-10-05 10:46:56 -0700

    Moar methods

--- rgrep.c
+++ rgrep.c
@@ -100,30 +100,14 @@ int questionMatch(char *line, int lineCursor, char whatToLookFor, int initial)
 	return lineCursor;
 }
 
-/**
- * You may assume that all strings are properly null terminated 
- * and will not overrun the buffer set by MAXSIZE 
- *
- * Implementation of the rgrep matcher function
-Student note: main calls rgreg_matches for _each line._
- */
-int rgrep_matches(char *line, char *pattern) {
-
-	/**First we're gonna split up our pattern	*/
-	
-	/**We search the line for each part of the pattern
-	We maintain line "cursor" position as we iterate through each section of the pattern (result must be contiguous, obviously)
-	*/
-
-	int lineCursor = 0;
-	int patternCursor = 0;
-	int match = 0; //Are we currently matching (used when we hit end of line
-	int attempts = 0; //Used to reset lineCursor to when "resetting" search
-	int initial = 1; //Is this the first character we're looking for?
-	while (1)
-	{
-		/**If we're at the end of the pattern and we are matching, return now
+int theMatcher(char *line, char *pattern, int lineCursor, int patternCursor)
+{
+	/**If we're at the end of the pattern and we are matching, return now
 		Otherwise, reset patternCursor*/
+	int initial = 1; //Is this the first character we're looking for?
+	int match = 0; //Are we currently matching (used when we hit end of line)
+	while(1)
+	{
 		if (!pattern[patternCursor])
 		{
 			if (match)
@@ -204,20 +188,40 @@ int rgrep_matches(char *line, char *pattern) {
 		Also reset line "cursor" position + amount of attempts
 		*/
 		else
-		{
-			patternCursor = 0;
-			attempts++;
-			lineCursor = attempts;
-			match = 0;
-			initial = 1;
-		}
+			return 0;
+	}
 		//? = return same value/position if not found
 		//+ = if found, keep check if next character also matches. Continue process, and return appropriate position
 		//+? = same as ?, but if found, apply + (call ? method, if it doesn't return same position, call +)
-		
+}
+/**
+ * You may assume that all strings are properly null terminated 
+ * and will not overrun the buffer set by MAXSIZE 
+ *
+ * Implementation of the rgrep matcher function
+Student note: main calls rgreg_matches for _each line._
+ */
+int rgrep_matches(char *line, char *pattern) {
+
+	/**First we're gonna split up our pattern	*/
+	
+	/**We search the line for each part of the pattern
+	We maintain line "cursor" position as we iterate through each section of the pattern (result must be contiguous, obviously)
+	*/
+
+	int lineCursor = 0;
+	int patternCursor = 0;
+	int attempts = 0; //Used to reset lineCursor to when "resetting" search
+	int	matching = theMatcher(line, pattern, lineCursor, patternCursor);
+	while (matching == -1)
+	{
+		patternCursor = 0;
+		attempts++;
+		lineCursor = attempts;
+		matching = theMatcher(line, pattern, lineCursor, patternCursor);
 	}
 
-    return match;
+    return matching;
 }
 
 int main(int argc, char **argv) {
